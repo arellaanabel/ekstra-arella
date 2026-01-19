223203c13a92ed04b5231df0379fc72d05a92062
Arella Elizia Anabel <arellaeliziaanabel@gmail.com>
2026-01-19 13:31:36 +0700

    Update database connection details in koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_ekskul";
+$host = "database-arella.csgirdm2h4lv.us-east-1.rds.amazonaws.com";
+$user = "admin";
+$pass = "admin123";
+$db   = "arellaDB";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
